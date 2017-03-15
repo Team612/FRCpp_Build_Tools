861952a25acabc3415711c57789f59b2648151a3
Ahmad-Bamba <ahmad.eb.bamba@gmail.com>
2017-03-15 09:56:27 -0400

    Organize scripts

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 all:
-	bash script/build.sh
+	bash script/unix/build.sh
+
+competition:
+	bash script/unix/compbuild.sh
 
 verbose:
-	bash script/build.sh -v
+	bash script/unix/build.sh -v
 
 deploy:
-	bash script/deploy.sh
+	bash script/unix/deploy.sh
 
 update:
-	sh script/.wpilib-download.sh
-	sh script/.get-ctre.sh
+	sh script/unix/.wpilib-download.sh
+	sh script/unix/.get-ctre.sh
 
 clean:
 	rm -rf .build/CMakeFiles/ .build/cmake_install.cmake .build/CMakeCache.txt .build/FRCUserProgram .build/Makefile
--- script/unix/deploy.sh
+++ script/unix/deploy.sh
@@ -4,19 +4,23 @@
 #make
 
 echo "deploy.sh: Beginning deploy process..."
-team=$(cat TEAM_NAME)
+echo "Enter team name: "
+read team
 printf "Team $team\n"
 team_ip=""
 os=$(uname | tr '[:upper:]' '[:lower:]')
 
 if [ ${#team} -eq 4 ]; then
     team_ip="${team:0:2}.${team:2}"
-    printf "Team IP 10.$team_ip.20\n"
+    printf "Team IP 10.$team_ip.2\n"
     printf "Operating system: $os\n"
 elif [ ${#team} -eq 3 ]; then
     team_ip="${team:0:1}.${team:1}"
-    printf "Team IP 10.$team_ip.20\n"
+    printf "Team IP 10.$team_ip.2\n"
     printf "Operating system: $os\n"
+else
+    printf "Invalid team name!"
+    exit 1
 fi
 
 if [ ! -f ".build/FRCUserProgram" ]; then
@@ -24,16 +28,16 @@ if [ ! -f ".build/FRCUserProgram" ]; then
     exit 1
 fi
 
-if [[ ! "$os" == *"cygwin"* ]]; then
+if [[ ! "$os" == *"cygwin"* || ! "$os" == *"nt"* ]]; then
     ping -c 4 "roboRIO-$team-FRC.local" ; mDNS=$?
     if [ $mDNS -ne 0 ]; then
         echo "roboRIO not found on mDNS, falling back to static USB..."
         ping -c 4 "172.22.11.2" ; static=$?
         if [ $static -ne 0 ] ; then
             echo "Failed to connect via static USB, falling back to guessing static ethernet..."
-            ping -c 4 "10.$team_ip.20" ; ethernet=$?
+            ping -c 4 "10.$team_ip.2" ; ethernet=$?
             if [ $ethernet -ne 0 ] ; then
-                echo "Connection failed on guess: 10.$team_ip.20!"
+                echo "Connection failed on guess: 10.$team_ip.2!"
                 echo "Connection to roboRIO failed on all points!"
             else
                 ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
@@ -57,9 +61,9 @@ else
         ping -n 4 "172.22.11.2" ; static=$?
         if [ $static -ne 0 ] ; then
             echo "Failed to connect via static USB, falling back to guessing static ethernet..."
-            ping -n 4 "10.$team_ip.20" ; ethernet=$?
+            ping -n 4 "10.$team_ip.2" ; ethernet=$?
             if [ $ethernet -ne 0 ] ; then
-                echo "Connection failed on guess: 10.$team_ip.20!"
+                echo "Connection failed on guess: 10.$team_ip.2!"
                 echo "Connection to roboRIO failed on all points!"
             else
                 ssh "lvuser@10.$team_ip.2" "killall -9 FRCUserProgram && rm ~/FRCUserProgram && exit"
